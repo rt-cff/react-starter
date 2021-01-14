63d45cb30dba1f8a89be419c55905d51f8c44c2b
Raymond Tam <raymondtamtwtp@gmail.com>
2021-01-14 14:52:35 +0800

    feat(pages): migrate to ts

---
+++ packages/next-app/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import type { AppProps } from 'next/app';
+
+import '../styles/globals.css';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+
+export default MyApp;
---
+++ packages/next-app/src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import { ReactElement } from 'react';
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = (): ReturnType<typeof originalRenderPage> =>
+        originalRenderPage({
+          enhanceApp: App => (props): ReactElement => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
