00a602409cd6cfc6619fffb3a557b2bf902349d1
Raymond Tam <raymondtamtwtp@gmail.com>
2021-01-15 18:01:30 +0800

    feat(next-app/components): [PageContainer] to use Page from core package

--- packages/next-app/src/app/components/page-container.tsx
+++ packages/next-app/src/app/components/page-container.tsx
@@ -1,7 +1,12 @@
 import { FC } from 'react';
 import type { AppProps } from 'next/app';
+import { Page } from '@onr/core';
 
 export const PageContianer: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   //TODO: add your custom rendering logic or props here
-  return <Component {...pageProps} />;
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
 };
